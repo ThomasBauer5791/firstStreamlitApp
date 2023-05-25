98ac81972f0ea29670c8e21a46188e74f3938ce0
Thomas Bauer <119656952+ThomasBauer5791@users.noreply.github.com>
2023-05-25 12:33:44 +0200

    Update streamlit_app.py
    added Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,7 @@
 import streamlit
 
 streamlit.title('First Test App')
+streamlit.header('🥣 Breakfast Menu')
+streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')

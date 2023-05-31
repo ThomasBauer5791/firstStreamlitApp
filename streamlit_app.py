20109f841f9c4cc37025cac016d22305647bbb14
Thomas Bauer <119656952+ThomasBauer5791@users.noreply.github.com>
2023-05-31 09:26:40 +0200

    Update streamlit_app.py
    normalization of JSON output added

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,10 @@ streamlit.dataframe(fruits_to_show)
 # new section to display response of Fruityvice API (http  requests)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# streamlit.text(fruityvice_response.json())
+# displaying the JSON as a table
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.dataframe(fruityvice_normalized)
 
 
 
